241597dc40b33e335e72670a763858e81fa1ec7c
Samantha Nicoll <85895313+SamanthaNicoll@users.noreply.github.com>
2022-05-04 12:01:24 -0600

    Add files via upload

---
+++ Domain/Interfaces/IOpenWeatherWeb.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IOpenWeatherWeb
+    {
+        Task<WeatherForeCast.ForeCastInfo> GetWeatherByGeo(double x, double y, long dt);
+        Task<List<coordenadas>> GetLatLong(string city);
+    }
+}
